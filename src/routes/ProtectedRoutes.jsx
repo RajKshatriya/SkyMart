c0c726473138af535f2544547212d6308ab3556b
Rajkshatriya <rajkshatriya1960@hmail.com>
2026-07-24 01:02:43 +0530

    fix protected routes and import paths

--- src/routes/ProtectedRoutes.jsx
+++ src/routes/ProtectedRoutes.jsx
@@ -3,9 +3,9 @@ import { Navigate, Outlet } from 'react-router';
 import { useAuthContext } from '../context/AuthContext';
 
 const ProtectedRoutes = () => {
-    const { loggedInUser } = useAuthContext();
+  const { loggedInUser } = useAuthContext();
 
-    return loggedInUser ? <Outlet /> : <Navigate to="/" replace />;
+  return loggedInUser ? <Outlet /> : <Navigate to="/" replace />;
 };
 
 export default ProtectedRoutes;
